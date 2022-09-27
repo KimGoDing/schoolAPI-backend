7a3ff532c76f0b634d458edc586c0d60e7833e9f
Hott-J <jhj13062004@naver.com>
2022-09-27 20:10:27 +0900

    init

---
+++ src/main/java/com/example/schoolapibackend/SchoolApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolapibackend/SchoolApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
